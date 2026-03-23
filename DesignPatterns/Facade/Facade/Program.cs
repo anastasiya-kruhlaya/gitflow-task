02e5d9bca5ad3e4083b5d7b7db923a8352395061
Dziyana Haradzetskaya <diana21032006w@gmail.com>
2026-03-23 06:53:39 +0300

    add facade pattern

---
+++ DesignPatterns/Facade/Facade/Program.cs
@@ -0,0 +1,16 @@
+﻿using Facade;
+
+class Program
+{
+    static void Main()
+    {
+        var simpleConverter = new SimpleConverter();
+
+        string result1 = simpleConverter.Convert("myvideo.avi", "MP4");
+        Console.WriteLine($"Conversion complete: {result1}\n");
+
+        string result2 = simpleConverter.Convert("anothervideo.mov", "OGG");
+        Console.WriteLine($"Conversion complete: {result2}");
+
+    }
+}
---
+++ DesignPatterns/Facade/Facade/SimpleConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    public class SimpleConverter
+    {
+        private readonly VideoConverter _videoConverter;
+        public SimpleConverter()
+        {
+            _videoConverter = new VideoConverter();
+        } 
+
+        public string Convert(string filename, string format)
+        {
+            var videoFile = new VideoFile(filename);
+
+            Codec codec = format.ToUpper() switch
+            {
+                "MP4" => new MPEG4Codec(),
+                "OGG" => new OGGCodec(),
+                _ => throw new NotSupportedException($"Format '{format}' is not supported")
+            };
+
+            _videoConverter.Convert(videoFile, codec);
+
+            string outputFilename = Path.GetFileNameWithoutExtension(filename) +
+                                    $"_converted.{format.ToLower()}";
+            return outputFilename;
+        }
+    }
+}
