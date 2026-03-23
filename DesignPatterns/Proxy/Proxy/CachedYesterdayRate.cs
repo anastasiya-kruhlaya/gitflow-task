f4b1754045c42b7cf9d8c7214774c619c499bd35
Dziyana Haradzetskaya <diana21032006w@gmail.com>
2026-03-23 07:43:56 +0300

    add proxy design pattern

---
+++ DesignPatterns/Proxy/Proxy/CachedYesterdayRate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    public class CachedYesterdayRate:IYesterdayRate
+    {
+        private IYesterdayRate _realYesterdayRate;
+        private decimal? _cachedRate;
+        public CachedYesterdayRate()
+        {
+            _realYesterdayRate = new YesterdayRate(); 
+        }
+        public decimal GetRate()
+        {
+            if (_cachedRate.HasValue)
+            {
+                Console.WriteLine("Return from cache");
+                return _cachedRate.Value;
+            }
+
+            Console.WriteLine("Cache is null, making a request");
+            _cachedRate = _realYesterdayRate.GetRate();
+            return _cachedRate.Value;
+        }
+    }
+}
---
+++ DesignPatterns/Proxy/Proxy/Program.cs
@@ -0,0 +1,16 @@
+﻿using Proxy;
+
+class Program
+{
+    static void Main()
+    {
+
+        IYesterdayRate rateService = new CachedYesterdayRate();
+
+        decimal rate1 = rateService.GetRate();
+        Console.WriteLine($"Exchange rate: {rate1}\n");
+
+        decimal rate2 = rateService.GetRate();
+        Console.WriteLine($"Exchange rate: {rate2}\n");
+    }
+}
