041dbd9757e1ce77960f6f5ca7581f6e0ee8c432
Dziyana Haradzetskaya <diana21032006w@gmail.com>
2026-03-23 09:27:13 +0300

    add singleton design pattern

---
+++ DesignPatterns/Singleton/Singleton/Database.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Database
+    {
+        public List<Book> Books {  get; set; }
+        private Database()
+        {
+            Books = new List<Book>();
+        }
+        public override string ToString()
+        {
+            var output = new StringBuilder();
+            foreach (Book book in Books)
+            {
+                output.AppendLine(book.ToString());
+            }
+            return output.ToString();
+        }
+        private static Database databaseInstatce = new Database();
+        public static Database PublicDatabaseInstance => databaseInstatce;
+    }
+}
